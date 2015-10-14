9da31d200cae8804ef5a0f6bb01151c374380216
ChBoler <chboler@hotmail.com>
2015-10-13 23:43:31 -0400

    Entry Field Progress
    Additions to the data fields for manual returns; working data saving
    from fields to variables

--- app/src/main/java/mapleleafstrings/mapleleafapp/ManualReturnActivity.java
+++ app/src/main/java/mapleleafstrings/mapleleafapp/ManualReturnActivity.java
@@ -1,35 +1,36 @@
 package mapleleafstrings.mapleleafapp;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.InputType;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AbsListView;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Calendar;
-import java.util.Objects;
+import java.util.*;
 
 public class ManualReturnActivity extends AppCompatActivity {
 
@@ -38,6 +39,7 @@ public class ManualReturnActivity extends AppCompatActivity {
     boolean keyboardNextIsPressed;
     String currentDate;
     LinearLayout linearLayout;
+
     String recievedDate, recievedBy, recievedFrom, carrier, boxNumber,
             boxDimensions, damageDescription, returnReason;
     String[] trackingNumbers, privateLabels, returnedItems, serialNumbers;
@@ -46,6 +48,7 @@ public class ManualReturnActivity extends AppCompatActivity {
     HashMap<String, Integer> fieldIDList = new HashMap<String, Integer>(){{
         put("byField", 1000);
         put("fromField", 1001);
+        put("dateField", 1002);
     }};
 
     // A list of data field pages to make
@@ -137,6 +140,8 @@ public class ManualReturnActivity extends AppCompatActivity {
             // Date field; calls a date picker dialog when selected
             dp = new EditText(this);
             dp.setInputType(InputType.TYPE_NULL);
+            dp.setId(fieldIDList.get("dateField"));
+
             // Listen for selection to bring up a date select menu
             dp.setOnFocusChangeListener(new View.OnFocusChangeListener() {
                 @Override
@@ -171,7 +176,6 @@ public class ManualReturnActivity extends AppCompatActivity {
             setSingleLineRules(rb);
             rb.setId(fieldIDList.get("byField"));
             linearLayout.addView(rb);
-            int testVar = rb.getId();
 
             // Received From label
             TextView ft = new TextView(this);
@@ -190,17 +194,115 @@ public class ManualReturnActivity extends AppCompatActivity {
             t.setText("Shipping Carrier for Returned Package:");
             linearLayout.addView(t);
 
+            // Carrier radio button
+            final RadioButton[] crb = new RadioButton[3];
+            RadioGroup crg = new RadioGroup(this);
+            crg.setOrientation(RadioGroup.HORIZONTAL);
+
+            crb[0] = new RadioButton(this);
+            crg.addView(crb[0]);
+            crb[0].setText("UPS");
+
+            crb[1] = new RadioButton(this);
+            crg.addView(crb[1]);
+            crb[1].setText("Conway");
+
+            crb[2] = new RadioButton(this);
+            crg.addView(crb[2]);
+            crb[2].setText("Other");
+
+            linearLayout.addView(crg);
+
+            // Box Number label
+            TextView nt = new TextView(this);
+            nt.setText("Number of boxes returned:");
+            linearLayout.addView(nt);
+
+            // Box Number field
+            EditText nf = new EditText(this);
+            nf.setInputType(InputType.TYPE_CLASS_NUMBER);
+            linearLayout.addView(nf);
+
+            // Tracking Number label
+            TextView tt = new TextView(this);
+            tt.setText("Input Tracking Numbers:");
+            linearLayout.addView(tt);
+
+            // Add a sub-layout with two buttons for adding and removing from the table
+            LinearLayout buttonSubLayout = new LinearLayout(this);
+            buttonSubLayout.setClickable(true);
+            buttonSubLayout.setFocusableInTouchMode(true);
+
+            Button addButton = new Button(this);
+            addButton.setText("Add");
+            buttonSubLayout.addView(addButton);
+
+            Button removeButton = new Button(this);
+            removeButton.setText("Remove");
+            buttonSubLayout.addView(removeButton);
+
+            linearLayout.addView(buttonSubLayout);
+
+            // Tracking Number table
+            EditText tl = new EditText(this);
+            setTextTableRules(tl);
+            linearLayout.addView(tl);
+
         } else if(field == "BoxDimensions"){
             // Dimensions label
             TextView t = new TextView(this);
             t.setText("(Optional) Specify the dimensions of the returned boxes:");
             linearLayout.addView(t);
 
+            // New linear layout for dimensions
+            LinearLayout dimensionLayout = new LinearLayout(this);
+            dimensionLayout.setOrientation(LinearLayout.HORIZONTAL);
+            dimensionLayout.setClickable(true);
+            dimensionLayout.setFocusable(true);
+            dimensionLayout.setGravity(Gravity.CENTER);
+
+            // Field 1
+            EditText e1 = new EditText(this);
+            e1.setWidth(100);
+            setSingleLineRules(e1);
+            dimensionLayout.addView(e1);
+
+            // Label 1
+            TextView l1 = new TextView(this);
+            l1.setText("x");
+            dimensionLayout.addView(l1);
+
+            // Field 2
+            EditText e2 = new EditText(this);
+            e2.setWidth(100);
+            setSingleLineRules(e2);
+            dimensionLayout.addView(e2);
+
+            // Label 2
+            TextView l2 = new TextView(this);
+            l2.setText("x");
+            dimensionLayout.addView(l2);
+
+            // Field 3
+            EditText e3 = new EditText(this);
+            e3.setWidth(100);
+            setSingleLineRules(e3);
+            dimensionLayout.addView(e3);
+
+            linearLayout.addView(dimensionLayout);
+
         } else if(field == "IsDamaged"){
             // Damage Confirmation label
             TextView t = new TextView(this);
             t.setText("Is the return damaged in any way?");
             linearLayout.addView(t);
+
+            // Damage Confirmation buttons
+            LinearLayout damageLayout = new LinearLayout(this);
+            damageLayout.setOrientation(LinearLayout.HORIZONTAL);
+            damageLayout.setClickable(true);
+            damageLayout.setFocusable(true);
+            damageLayout.setGravity(Gravity.CENTER);
 
             // Increment the index by an additional amount if "no" is selected
 
@@ -217,16 +319,68 @@ public class ManualReturnActivity extends AppCompatActivity {
             linearLayout.addView(t);
 
         } else if(field == "Items Returned and Serials"){
-            // Items Returned label
+            // Items Returned and Serials label
             TextView t = new TextView(this);
-            t.setText("List the items that are being returned below:");
+            t.setText("List the items that are being returned below, and any applicable serial numbers:");
             linearLayout.addView(t);
+
+            // Add a sub-layout with two buttons for adding and removing from the table
+            LinearLayout buttonSubLayout = new LinearLayout(this);
+            buttonSubLayout.setClickable(true);
+            buttonSubLayout.setFocusableInTouchMode(true);
+
+            Button addButton = new Button(this);
+            addButton.setText("Add");
+            buttonSubLayout.addView(addButton);
+
+            Button removeButton = new Button(this);
+            removeButton.setText("Remove");
+            buttonSubLayout.addView(removeButton);
+
+            linearLayout.addView(buttonSubLayout);
+
+            // Items Returned and Serials table
+            // List serial number below each returned item, even if empty
+            EditText ise = new EditText(this);
+            setTextTableRules(ise);
+            linearLayout.addView(ise);
 
         } else if(field == "Reason For Return") {
             // Reason Returned label
             TextView t = new TextView(this);
             t.setText("Describe the reason for the return, if known (otherwise just put 'unknown'):");
             linearLayout.addView(t);
+
+            // Reason Returned text edit
+            EditText re = new EditText(this);
+            re.setMaxLines(10);
+            re.setLines(10);
+
+            // Hacky way of making sure the keyboard isn't hidden when Next is pressed.
+            // Can probably be done in a better way
+            re.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                @Override
+                public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                    if (actionId == EditorInfo.IME_ACTION_NEXT) {
+                        keyboardNextIsPressed = true;
+                    } else {
+                        keyboardNextIsPressed = false;
+                    }
+                    return false;
+                }
+            });
+
+            // Listen for when the area outside of the keyboard is pressed
+            re.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if (!hasFocus && !keyboardNextIsPressed) {
+                        hideKeyboard(v);
+                    } else {
+                        keyboardNextIsPressed = false;
+                    }
+                }
+            });
 
         } else if(field == "Return Summary"){
             // Summary label
@@ -243,7 +397,7 @@ public class ManualReturnActivity extends AppCompatActivity {
     public Boolean allDataFieldsPopulated(){
         int layoutChildren = linearLayout.getChildCount();
 
-        // Make sure the linearLayout field is not empty
+        // Make sure the relativeLayout field is not empty
         if(layoutChildren > 0){
             // Iterate through all elements for any data entry
             // fields
@@ -266,18 +420,17 @@ public class ManualReturnActivity extends AppCompatActivity {
                             // Integer ID of the iterated hashmap segment
                             int testI = e.getValue();
 
-                            // Integer ID of the current EditText object
-                            EditText textObject = (EditText)v;
-                            int testI2 = v.getId();
-                            int testI3 = textObject.getId();
-                            int junkVar;
+                            // Compare the IDs to see if there is a match in the list
                             if (((EditText) v).getId() == e.getValue()){
                                 fieldString = e.getKey();
                             }
                         }
 
-                        //fieldString = (fieldIDList.get(((EditText) v).getId())).getKey();
-                        saveDataFields(fieldString);
+                        if (fieldString != "") {
+                            saveDataFields(fieldString);
+                        } else {
+                            //TODO: Error Handling
+                        }
                     }
                 }
             }
@@ -297,6 +450,9 @@ public class ManualReturnActivity extends AppCompatActivity {
                                 break;
             case "fromField":   recievedFrom = etObject.getText().toString().trim();
                                 displayToastMessage("fromField saved as " + recievedFrom);
+                                break;
+            case "dateField":   recievedDate = etObject.getText().toString().trim();
+                                displayToastMessage("dateField saved as " + recievedDate);
                                 break;
             default:            //TODO: Error Handling
                                 break;
@@ -379,7 +535,7 @@ public class ManualReturnActivity extends AppCompatActivity {
         textfield.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_NEXT){
+                if (actionId == EditorInfo.IME_ACTION_NEXT) {
                     keyboardNextIsPressed = true;
                 } else {
                     keyboardNextIsPressed = false;
@@ -389,10 +545,10 @@ public class ManualReturnActivity extends AppCompatActivity {
         });
 
         // Listen for when the area outside of the keyboard is pressed
-        textfield.setOnFocusChangeListener(new View.OnFocusChangeListener(){
+        textfield.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
-            public void onFocusChange(View v, boolean hasFocus){
-                if (!hasFocus && !keyboardNextIsPressed){
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus && !keyboardNextIsPressed) {
                     hideKeyboard(v);
                 } else {
                     keyboardNextIsPressed = false;
@@ -400,6 +556,19 @@ public class ManualReturnActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    // Set rules for EditText objects set up to behave as text display tables
+    public void setTextTableRules(EditText textfield){
+        // Prevent entering data
+        textfield.setInputType(InputType.TYPE_NULL);
+
+        // Set the tables size
+        textfield.setMaxLines(9);
+        textfield.setLines(9);
+
+        // Change the style of the field, to make it look more like a table
+        textfield.setBackgroundResource(R.drawable.table_stylesheet);
     }
 
     public void hideKeyboard(View view){
